1a5e8f78dae2952fb8e3cc8bda9e9d9394df48e8
pinto.breno <breno.pedro.a.pinto@accenture.com>
2025-03-14 10:29:45 -0300

    initial commit

---
+++ src/test/java/com/tds/url/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
